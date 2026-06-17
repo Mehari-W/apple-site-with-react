6580258424ec71d540a706c6c50e1fe467805faa
Mehari Woldegebrel <mehariwgebrel2@gmail.com>
2026-06-17 11:24:26 +0300

    Update Header.jsx

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import icon from "../../assets/images/icons/logo-sm.png";
 import search from "../../assets/images/icons/search-icon-sm.png";
 import cart from "../../assets/images/icons/cart-sm.png";
 
 const Header = () => {
+  // የሜኑውን ክፍት/ዝግ መሆን የሚቆጣጠር State
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // ሜኑውን Toggle የሚያደርግ ፊንክሽን
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <div className="nav-wrapper fixed-top">
         <div className="container">
           <nav className="navbar navbar-expand-md navbar-dark">
-            {/* Updated data attributes to data-bs for modern Bootstrap functionality */}
+            
             <button
               className="navbar-toggler"
               type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#appleNavbar"
+              onClick={toggleMenu} // የ onClick ኢቨንት ተጨምሯል
               aria-controls="appleNavbar"
-              aria-expanded="false"
+              aria-expanded={isMenuOpen} // Stateን መሰረት ያደረገ
               aria-label="Toggle navigation"
             >
-              {/* Swapped "☰" for the standard Bootstrap icon that animates clean and natively */}
               <span className="navbar-toggler-icon"></span>
             </button>
 
@@ -27,8 +33,11 @@ const Header = () => {
               <img src={icon} alt="Apple Logo" />
             </a>
 
-            {/* Added matching ID here to link up perfectly with the toggle button */}
-            <div className="navbar-collapse collapse" id="appleNavbar">
+            {/* isMenuOpen 'true' ሲሆን 'show' የሚለውን ክላስ ይጨምራል */}
+            <div 
+              className={`navbar-collapse collapse ${isMenuOpen ? "show" : ""}`} 
+              id="appleNavbar"
+            >
               <ul className="navbar-nav nav-justified w-100 nav-fill">
                 <li className="nav-item">
                   <a className="nav-link js-scroll-trigger" href="#mac/">
